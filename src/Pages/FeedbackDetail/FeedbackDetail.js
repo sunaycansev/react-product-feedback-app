0a0eff01d9370662a1cd2f76f537b9fa130b473b
sunaycansev <fsunaycansev@gmail.com>
2022-03-20 14:37:36 +0300

    fix: product detail page view

--- src/Pages/FeedbackDetail/FeedbackDetail.js
+++ src/Pages/FeedbackDetail/FeedbackDetail.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { ArrowLeftIcon } from '@heroicons/react/solid';
+import Feedback from '../../Components/Feedback';
 import { getSingleFeedback } from '../../Services/Feedback';
 
 function FeedbackDetail() {
@@ -11,7 +13,7 @@ function FeedbackDetail() {
       try {
         const data = await getSingleFeedback(id);
 
-        setFeedback(data.toJSON());
+        setFeedback(data);
       } catch (e) {
         console.log(e);
       }
@@ -19,12 +21,38 @@ function FeedbackDetail() {
     getFeedback();
   }, [id]);
 
+  // TODO add loading state
+
+  if (!feedback) {
+    return 'loading';
+  }
+
   return (
-    <div>
-      <div>{feedback.title}</div>
-      <div>{feedback.category}</div>
-      <div>{feedback.upvotes}</div>
-      <div>{feedback.details}</div>
+    <div className="page flex justify-center items-center mt-10">
+      <div className="container flex flex-col justify-center px-10   bg-blue-300">
+        <div className="back">
+          <Link to="/">
+            <div className="back my-10  text-center font-extrabold text-xl flex justify-center items-center transition ease-in-out duration-500 hover:underline hover:cursor-pointer hover:translate-y-1 hover:text-fuchsia-600 ">
+              <ArrowLeftIcon className="w-5 h-5 mr-3" />
+              <span>Go back</span>
+            </div>
+          </Link>
+        </div>
+        <div className="content">
+          <div className="feedback">
+            <Feedback feedback={feedback} />
+          </div>
+          <div className="comments">a</div>
+          <div className="add-comment">
+            <div className="add-comment_heading">a</div>
+            <div className="add-comment_textarea">a</div>
+            <div className="add-comment_bottom_container">
+              <div className="letter-count">a</div>
+              <div className="submit-btn">a</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
